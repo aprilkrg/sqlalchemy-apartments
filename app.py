c72732878891ca8adf6b2480d1425d5e4c23e6a6
aprilkrgonzales <april.gonzales@generalassemb.ly>
2022-01-05 16:08:39 -0700

    complete part one and merge to main

--- app.py
+++ app.py
@@ -57,58 +57,11 @@ def apt_test():
 app.route('/apt_test', methods=['GET'])(apt_test)
 
 def make_apt():
-    # Insert the following apartments
-    # Archstone - 20 units
-    # Zenith Hills - 10 units
-    # Willowspring - 30 units
-
-    # attempt 1, modeled off previous 
-    # apts = models.Apartment.insert().values([
-    #     {"name": "Archstone", "units": 20},
-    #     {"name": "Zenith Hills", "units": 10},
-    #     {"name": "Willowspring", "units": 30}
-    # ])
-    # models.db.session.add(apts)
-
-    # attempt 2, after 'Apartment' has no attribute 'insert' error tried to be more direct
-    # models.db.session.add([
-    #     {"name": "Archstone", "units": 20},
-    #     {"name": "Zenith Hills", "units": 10},
-    #     {"name": "Willowspring", "units": 30}
-    # ])
-
-    # attempt 3, got no attribute insert error
-    # apt = models.Apartment.insert().values(
-    #     {"name": "Archstone"}
-    # )
-
-    # attempt 4, no attribute values
-    # apt = models.Apartment.values(
-    #     {"name": "Archstone"}
-    # )
-
-    #  attempt 5, worked!
-    # apt = models.Apartment(
-    #     name='Archstone',
-    #     units=20, 
-    #     owner_id=5
-    # )
-    # models.db.session.add(apt)
-
-    # attempt 6, bulk insert did not work
-    # apts = models.Apartment([
-    #     {'name':'Willowspring','units':30, 'owner_id':5},
-    #     {'name':'Zenith Height', 'units': 10, 'owner_id':6}
-    # ])
-    # models.db.session.add(apts)
-
-    # attempt 7, builtins.list is not mapped ??
-    # models.db.session.add([
-    #     {'name':'Willowspring','units':30, 'owner_id':5},
-    #     {'name':'Zenith Height', 'units': 10, 'owner_id':6}
-    # ])
-
-    apts_to_insert = [models.Apartment(name='Zenith Hills',units=10,owner_id=5), models.Apartment(name='Willowspring',units=30,owner_id=6), models.Apartment(name='Hilltop',units=200,owner_id=10)]
+    apts_to_insert = [
+        models.Apartment(name='Zenith Hills',units=10,owner_id=5), 
+        models.Apartment(name='Willowspring',units=30,owner_id=6), 
+        models.Apartment(name='Hilltop',units=200,owner_id=10)
+    ]
     models.db.session.bulk_save_objects(apts_to_insert)
     models.db.session.commit()
     apartments = models.Apartment.query.all()
